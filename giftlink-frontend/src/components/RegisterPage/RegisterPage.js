589644447f253f99055806e75faec3ed5d808800
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-01-28 01:19:47 +0100

    Back-end in Register Component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 
+// Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import { urlConfig } from "../../config";
+
+// Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useAppContext } from "../../context/AuthContext";
+
+// Task 3: Import useNavigate from `react-router-dom` to handle navigation after successful registration.
+import { useNavigate } from "react-router-dom";
+
 import "./RegisterPage.css";
 
 function RegisterPage() {
@@ -9,9 +18,57 @@ function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // insert code here to create handleRegister function and include console.log
+  // Task 4: Include a state for error message.
+  const [showerr, setShowerr] = useState("");
+
+  // Task 5: Create a local variable for `navigate` and `setIsLoggedIn`.
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useAppContext();
+
   const handleRegister = async () => {
-    console.log("Register invoked");
+    try {
+      const response = await fetch(
+        `${urlConfig.backendUrl}/api/auth/register`,
+        {
+          // Task 6: Set method
+          method: "POST",
+          // Task 7: Set headers
+          headers: {
+            "content-type": "application/json",
+          },
+          // Task 8: Set body to send user details
+          body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+          }),
+        }
+      );
+
+      // Task 1: Access data coming from fetch API
+      const json = await response.json();
+
+      // Task 2: Set user details
+      if (json.authtoken) {
+        sessionStorage.setItem("auth-token", json.authtoken);
+        sessionStorage.setItem("name", firstName);
+        sessionStorage.setItem("email", json.email);
+
+        // Task 3: Set the state of user to logged in using the `useAppContext`.
+        setIsLoggedIn(true);
+
+        // Task 4: Navigate to the MainPage after logging in.
+        navigate("/app");
+      }
+
+      // Task 5: Set an error message if the registration fails.
+      if (json.error) {
+        setShowerr(json.error);
+      }
+    } catch (e) {
+      console.log("Error fetching details: " + e.message);
+    }
   };
 
   return (
@@ -83,6 +140,9 @@ function RegisterPage() {
               />
             </div>
 
+            {/* Task 6: Display error message to enduser. */}
+            <div className="text-danger">{showerr}</div>
+
             {/* insert code here to create a button that performs the `handleRegister` function on click */}
             <button
               className="btn btn-primary w-100 mb-3"
@@ -90,7 +150,6 @@ function RegisterPage() {
             >
               Register
             </button>
-
             <p className="mt-4 text-center">
               Already a member?{" "}
               <a href="/app/login" className="text-primary">
